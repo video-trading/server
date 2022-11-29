eb36dce29436f9dc4d9aa58b7a236be2eb4a4b2e
sirily11 <sirily1997@gmail.com>
2022-11-30 01:48:37 +0800

    fix: import

--- src/transcoding/transcoding.module.ts
+++ src/transcoding/transcoding.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TranscodingService } from './transcoding.service';
 import { TranscodingController } from './transcoding.controller';
 import { PrismaService } from 'src/prisma.service';
+import { StorageService } from '../storage/storage.service';
 
 @Module({
   controllers: [TranscodingController],
-  providers: [PrismaService, TranscodingService],
+  providers: [PrismaService, TranscodingService, StorageService],
 })
 export class TranscodingModule {}
